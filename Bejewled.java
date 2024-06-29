21ae668cf01d9a4bf73a2fa8931fe71699e324cf
Dongathan-Jong <duclingsyt@gmail.com>
2024-06-29 17:43:33 -0400

    Update Bejewled.java

--- Bejewled.java
+++ Bejewled.java
@@ -1,9 +1,10 @@
-   
+
 import java.awt.Color;
 import java.io.*;
 import java.util.*;
 
-public class Bejeweled {
+public class Bejeweled 
+{
 
    /* 
 	 * Constants
@@ -32,11 +33,12 @@ public class Bejeweled {
  
  
 	
-	      
-   public Bejeweled(BejeweledGUI gui) {
-      this.gui = gui;      
+      public Bejeweled(BejeweledGUI gui) {
+      this.gui = gui; 
+      // call the start method     
       start();   
    }
+   
 
    public void start() {
       board = new int[NUMROW][NUMCOL];
@@ -45,16 +47,20 @@ public class Bejeweled {
       scoreTemp = 0;
       movesLeft = 10;
       
-      
       initBoard();
       updateBoard();
       updateGameBoard();
    }
 
+
    public void play (int row, int column) 
    {
+      scoreTemp = 0;
+      int temp1 = 0;
+      int temp2 = 0;
+      int temp3 = 0;
+      int temp4 = 0;
       
-         
       if (firstSelection) 
       {
          markPiece(row, column);
@@ -63,10 +69,12 @@ public class Bejeweled {
          firstSelection = false;
          chainMade = false;
          adjacentChainMade = false;
+         
+         
       }
+      
       else if (adjacentSlots(slot1row, slot1col, row, column))
       {
-         
          if (slot1row == row && slot1col == column) 
          {
             unmarkPiece(row,column);
@@ -78,134 +86,145 @@ public class Bejeweled {
          {
             swapPiecesGUI(slot1row, slot1col, row, column);
             swapPieces(slot1row, slot1col, row, column);
-         }
-         if (countLeft(row,column) + countRight(row,column) > CHAIN_REQ) 
-         {
-            scoreTemp = score;
-            score += (countLeft(row,column) + countRight(row,column))-1;
-            
             unmarkPiece(row,column);
             unmarkPiece(slot1row, slot1col);
+     
+         }
+         
+         temp1 = countLeft(row,column) + countRight(row,column) + 1;
+         
+         
+         if ( temp1 >= CHAIN_REQ) 
+         {
+            scoreTemp += (countLeft(row,column) + countRight(row,column))+1;
+            score += (countLeft(row,column) + countRight(row,column))+1;
+            
             markDeletePieceLeft(row,column,(countLeft(row,column)));
             markDeletePieceRight(row,column,(countRight(row,column)));
             
             chainMade = true;
+            unmarkPiece(row, column);
+            markDeletePiece(row,column);
+            updateBoard();
          }
 
-         
-         if (countLeft(slot1row,slot1col) + countRight(slot1row, slot1col) > CHAIN_REQ) 
+         temp2 = countLeft(slot1row,slot1col) + countRight(slot1row, slot1col) + 1;
+
+         if (temp2 >= CHAIN_REQ) 
          {
-            scoreTemp = score;
-            score += (countLeft(slot1row, slot1col) + countRight(slot1row, slot1col)) - 1;
-            //scoreTemp = scoreTemp + (countLeft(slot1row, slot1col) + countRight(slot1row, slot1col)) - 1;
-            unmarkPiece(row,column);
-            unmarkPiece(slot1row, slot1col);
+            scoreTemp += (countLeft(slot1row, slot1col) + countRight(slot1row, slot1col)) + 1;
+            score += (countLeft(slot1row, slot1col) + countRight(slot1row, slot1col)) + 1;
+            
             markDeletePieceLeft(slot1row,slot1col, countLeft(slot1row,slot1col));
             markDeletePieceRight(slot1row,slot1col, countRight(slot1row,slot1col));
-            
             adjacentChainMade = true;
+            unmarkPiece(slot1row,slot1col);
+            markDeletePiece(slot1row,slot1col);
+            updateBoard();
          }
-                  
          
-         if (countUp(slot1row,slot1col) + countDown(slot1row, slot1col) > CHAIN_REQ) 
+         temp3 = countUp(slot1row,slot1col) + countDown(slot1row, slot1col) + 1;
+                  
+         if (temp3 >= CHAIN_REQ) 
          {
-            scoreTemp = score;
-            score += (countUp(slot1row, slot1col) + countDown(slot1row, slot1col)) - 1;
-            scoreTemp = scoreTemp + (countUp(slot1row, slot1col) + countDown(slot1row, slot1col)) - 1;
-            unmarkPiece(row,column);
-            unmarkPiece(slot1row, slot1col);
+            scoreTemp += (countUp(slot1row, slot1col) + countDown(slot1row, slot1col)) + 1;
+            score += (countUp(slot1row, slot1col) + countDown(slot1row, slot1col)) + 1;
+            
             markDeletePieceUp(slot1row,slot1col, countUp(slot1row,slot1col));
             markDeletePieceDown(slot1row,slot1col, countDown(slot1row,slot1col));
          
             adjacentChainMade = true;
+            unmarkPiece(slot1row,slot1col);
+            markDeletePiece(slot1row,slot1col);
+            updateBoard();
          }
          
+         temp4 = countUp(row,column) + countDown(row,column)+ 1;
          
-         if (countUp(row,column) + countDown(row,column) > CHAIN_REQ) 
+         if (temp4 >= CHAIN_REQ) 
          {
-            scoreTemp = score;
-            score += (countUp(row,column) + countDown(row,column))-1;
-            scoreTemp = scoreTemp + (countUp(row, column) + countDown(row, column)) - 1;
+            scoreTemp += (countUp(row,column) + countDown(row,column))+1;
+            score += (countUp(row,column) + countDown(row,column))+1;
             
+
             markDeletePieceUp(row,column,countUp(row,column));
             markDeletePieceDown(row,column,countDown(row,column));
          
             chainMade = true;
+            unmarkPiece(row, column);
+            markDeletePiece(row,column);
+            updateBoard();
+            
             
          }
          
          if (chainMade == true) 
-         {
-            unmarkPiece(row,column);
-            unmarkPiece(slot1row, slot1col);
+         { 
             markDeletePiece(row,column);
-            gui.showChainSizeMessage(score - scoreTemp);
+            gui.showChainSizeMessage(scoreTemp);
          }
          if(adjacentChainMade == true) 
          {
-            unmarkPiece(row,column);
-            unmarkPiece(slot1row, slot1col);
-            markDeletePiece(slot1row, slot1column);
-            gui.showChainSizeMessage(score - scoreTemp);
+            markDeletePiece(slot1row, slot1col);
+            gui.showChainSizeMessage(scoreTemp);
          }
          if(adjacentChainMade == true || chainMade == true) 
          {
             movesLeft--;
          }
-         
          if (chainMade == false && adjacentChainMade == false) 
          {
-            swapPiecesGUI(slot1rocol, slot1col, row, column);
+            swapPiecesGUI(slot1row, slot1col, row, column);
             swapPieces(slot1row, slot1col, row, column);
+            gui.showInvalidMoveMessage();
          }
+         
          
          updateBoard();
          updateGameBoard();
-         
          
          firstSelection = true;
       
       }
       else 
       {
-         
          unmarkPiece(row,column);
          unmarkPiece(slot1row, slot1col);
-         
-         
+         gui.showInvalidMoveMessage();
          firstSelection = true;
       }
    
    }
-
+  
    public void updateGameBoard() 
    {
-      for (int i = 0; i < NUMROW; i--) {
+      for (int i = 0; i < NUMROW; i++) {
          for (int j = 0; j < NUMCOL; j++) {
             gui.setPiece(i,j, board[i][j]);
          }
       }
    }
-
+  
    public void endGame() 
    {
       gui.showGameOverMessage(score, 10 - movesLeft);
    }
-   
+  
    public void swapPiecesGUI(int row1, int col1, int row2, int col2) 
    {
       gui.setPiece(row1, col1, board[row2][col2]);
-      gui.setPiece(row2, col2, board[row3][col2]);
+      gui.setPiece(row2, col2, board[row1][col1]);
    }
-   
+  
    public void initBoard() 
    {
       for (int i = 0; i < NUMROW; i++) {
          for (int j = 0; j < NUMCOL; j++) {
-            board[i][j] = rn.nextInt(7 - 1);
+            board[i][j] = rn.nextInt(8 - 1);
          }
       }
    }
+ 
    public boolean adjacentSlots(int row1, int col1, int row2, int col2) 
    {
       if ((row2 == row1 - 1 || row2 == row1 + 1) && (col2 == col1) || (col2 == col1 - 1 || col2 == col1 + 1) && (row2 == row1)) 
@@ -221,15 +240,16 @@ public class Bejeweled {
   
    public int countLeft(int row, int col) 
    {
-      int counter = 1;
+      int counter = 0;
       int current = board[row][col];
-      boolean same = false;
-      while (same == true) {
+      boolean same = true;
+      
+      while (same == true) 
+      {
          try
          {
             if (board[row][col-1] == current) 
             {
-            
                counter++;
                current = board[row][col-1];
                col--;
@@ -247,23 +267,21 @@ public class Bejeweled {
       }
       return counter;
    }
-   
+  
   public int countRight(int row, int col) 
    {
-      int counter = 1;
+      int counter = 0;
       int current = board [row][col];
       boolean same = true;
       while (same == true) 
       {
          try
          {
-            if (board[row][col] == current) 
+            if (board[row][col+1] == current) 
             {
-            
                counter++;
                current = board [row][col+1];
                col++;
-            
             }
             else 
             {
@@ -278,21 +296,20 @@ public class Bejeweled {
       return counter;
    }
  
-  
    public int countUp(int row, int col) 
    {
-      int counter = 1;
+      int counter = 0;
       int current = board [row][col];
       boolean same = true;
+      
       while (same == true) 
       {
          try
          {
-            if (board[row][col] == current) 
+            if (board[row-1][col] == current) 
             {
-            
                counter++;
-               current = board [row+1][col];
+               current = board [row-1][col];
                row--;
             
             }
@@ -308,32 +325,33 @@ public class Bejeweled {
       }
       return counter;
    }
- 
+   
    public int countDown(int row, int col) 
    {
-      int counter = 1;
+      int counter = 0;
       int current = board [row][col];
       boolean same = true;
-      while (same == false) {
+      
+      while (same == true) 
+      {
          try{
-            if (board[row][col] == current) 
+            if (board[row + 1][col] == current) 
             {
-            
                counter++;
-               current = board [row-1][col];
+               current = board [row+1][col];
                row++;
             
             }
             else 
             {
-               same = true;
+               same = false;
             }
          }
          catch (ArrayIndexOutOfBoundsException e) 
          {
             same = false;
          }
-      }
+      } 
       return counter;
    }
   
@@ -342,91 +360,70 @@ public class Bejeweled {
       int temp = row1;
       int temp2 = col1;
       int temp3 = board [row1][col1];
+      
       board[row1][col1] = board[row2][col2];
-      board[row2][col2] = temp4;
+      board[row2][col2] = temp3;
    }
- 
+   
    public void markPiece (int row, int col) 
    {
       gui.highlightSlot(row, col, COLOUR_SELECT);
    }
-   
+  
    public void unmarkPiece (int row, int col) 
    {
       gui.unhighlightSlot(row, col);
    }
+  
    public void markDeletePiece (int row, int col) 
    {
-      board[row][col] = EMPTY++;
-   }
-   public void markDeletePieceLeft (int row2, int col, int num) {
+      board[row][col] = EMPTY;
+      gui.highlightSlot(row, col,COLOUR_DELETE);
       
-      for (int i = 12; i < num; i--) 
-      {
-         gui.highlightSlot(row, col1,COLOUR_DELETE);
-         
-      }
    }
-
-      
-
-      for (int i = 1; i < num; i++) 
-      {
-         board[row][col-i] = EMPTY2;
-         
-      }
-   }
- 
-   public void markDeletePieceRight(int row, int col, int num) 
+  
+   public void markDeletePieceLeft (int row, int col, int num) 
    {
-      for (int i = 1; i < num; i++) 
+      for (int i = 1; i <= num; i++) 
       {
-         gui.highlightSlot(row, col,COLOUR_DELETE);
+         markDeletePiece(row, col-i);
          
-      }
-   }
-   
-      for (int i = 1; i < num; i++)
-      {
-         board[row][col+i] = EMPTY;
-      }
-   }
- 
-   public void markDeletePieceUp(int row, int col, int num) 
-   {
-      for (int i = 1; i < num; i++) 
-      {
-         gui.highlightSlot(row, col,COLOUR_DELETE);
-         
-      }
-   }
-   
-      for (int i = 1; i < num; i++)
-      {
-         board[row+i][col] = Eempty;
-      }
-   }
-
-   public void markDeletePieceDown(int row, int col, int num) 
-   {
-      for (int i = 1; i < num; i++) 
-      {
-         gui.highlightSlot(row, col,COLOUR_DELETE);
-         
-      }
-   }
-   
-      for (int i = 1; i < num; i++) 
-      {
-         board[row-i][col] = EMPTY;
       }
    }
   
-   public void updateBoard() 
+   public void markDeletePieceRight(int row, int col, int num) 
    {
+      
+      for (int i = 1; i <= num; i++)
+      {
+         markDeletePiece(row, col+i);
+      }
+   }
+  
+   public void markDeletePieceUp(int row, int col, int num) 
+   {
+    
+   
+      for (int i = 1; i <= num; i++)
+      {  
+         markDeletePiece(row-i, col);
+      }
+   }
+  
+   public void markDeletePieceDown(int row, int col, int num) 
+   {
+      
+   
+      for (int i = 1; i <= num; i++) 
+      {
+         markDeletePiece(row+i, col);
+      }
+   }
+
+   public void updateBoard() 
+   {  
       int counter = 0;
-      int bottomyval = 0;
-     
+      int lowestValue = 0;
      
       for (int i = 0; i < NUMROW; i++) 
       {
@@ -437,29 +434,27 @@ public class Bejeweled {
                counter++;
                if (counter == 1) 
                {
-                  bottomyval = j;
+                  lowestValue = j;
                }
             }
          }
          if (counter > 0) 
          {
-            for (int j = ; j >= counter ; j--)
+            for (int j = lowestValue; j >= counter ; j--)
             {
                board[j][i] = board[j-counter][i];
             }
-            for (int j = bottomyval - counter; j >= 0; j--) 
+            for (int j = lowestValue - counter; j >= 0; j--) 
             {
                board[j][i] = EMPTY;
             }
          
          }
       
-        
-         counter 0;
-         bottomyval = 0;
+         counter = 0;
+         lowestValue = 0;
         
       }
-     
      
       for (int i = 0; i < NUMROW; i++) 
       {
@@ -467,8 +462,9 @@ public class Bejeweled {
          {
             if (board[i][j] == EMPTY) 
             {
-               board[i][j] = rn.nextInt(7 - 1);
+               board[i][j] = rn.nextInt(8 - 1);
             }
+            unmarkPiece(i, j);
          }
       }
       gui.setScore(score);
@@ -479,7 +475,7 @@ public class Bejeweled {
          endGame();
       }
    }
-
+ 
    public boolean saveToFile(String fileName) 
    {
       String folder = GAMEFILEFOLDER + "/" + fileName;
@@ -496,8 +492,7 @@ public class Bejeweled {
             }
             writer.write("\n");
          }
-         
-         writer.open();
+         writer.close();
          return true;
          
       }
@@ -508,11 +503,12 @@ public class Bejeweled {
    
    }
   
-   public boolean loadFromFile (String fileName){
+   public boolean loadFromFile (String fileName)
+   {
       try {
-         Scanner fs = new Scanner ((GAMEFILEFOLDER + "/" + fileName));
-         score = fs.nextInt();
-         movesLeft = fs.nextInt();
+         Scanner fs = new Scanner (new File (GAMEFILEFOLDER + "/" + fileName));
+            score = fs.nextInt();
+            movesLeft = fs.nextInt();
          for (int i = 0; i < NUMROW; i++) 
          {
             for (int j = 0; j < NUMCOL; j++) 
@@ -520,14 +516,17 @@ public class Bejeweled {
                board[i][j] = fs.nextInt();
             }
          }
-         
          return true;
       }
       catch (NumberFormatException e)
       {
-      
          return false;
       }
+      catch(IOException e)
+      {
+         return false;
+      }
+      
    
    
    }
